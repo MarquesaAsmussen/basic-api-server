9121f681c71462e22266d1c5f88ae904d7513207
Marquesa Asmussen <marquesa_a@yahoo.com>
2021-10-21 12:24:29 -0700

    update frame work and packag.json

--- README.md
+++ README.md
@@ -1,3 +1,26 @@
 # basic-api-server
 
-small update
+## Author
+
+Marquesa Asmussen
+
+## Summary of Problem Domain
+
+## Links to Application Deployment
+
+[Heroku](https://marquesa-basic-api-server.herokuapp.com/)
+
+[Actions Page](https://github.com/MarquesaAsmussen/basic-api-server/actions/new)
+
+[PR](https://github.com/MarquesaAsmussen/basic-api-server/pull/1)
+
+## Include Embedded UML
+
+<!-- <img src="./images/lab2-uml.png" alt="lab2-uml" /> -->
+
+## Routes
+
+<!-- - HTTP GET
+
+  - Path: /person
+    - Response with the name passed in the query string. -->
--- src/server.js
+++ src/server.js
@@ -1,27 +1,23 @@
 'use strict';
 
 const express = require('express');
-const { phrses } = require('../model');
+const { clothes } = require('./models/clothes');
+const { food } = require('./models/food');
+const { index } = require('./models/index');
 const app = express();
 
 app.use(express.json());
-app.use(logger);
-// see above note for variables
-app.use(handle404Error);
-app.use(handle500Error);
+// are these correct?
+app.use(food);
+app.use(clothes);
+app.use(index);
+
+// app.use(logger);
+// // see above note for variables
+// app.use(handle404Error);
+// app.use(handle500Error);
 
 app.get('/person', handlePerson);
-
-// this function needs work
-function handlePerson(req, res) {
-  let { person } = request.body;
-  // need to bring in validator here
-
-  // need to create a place to push this to
-  people.push(person);
-
-  response.send(person);
-}
 
 module.exports = {
   app,
